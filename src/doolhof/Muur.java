e3b7f35f91dbec5a54c4ad587f32921f1869d67f
Cees <>
2015-05-27 12:07:09 +0200

    muur

--- src/doolhof/Muur.java
+++ src/doolhof/Muur.java
@@ -1,57 +1,54 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package doolhof;
 
-/**
- *
+/*
  * @author http://forum.codecall.net/topic/63862-maze-tutorial/
  */
-public class Muur{
+public class Muur
+{   
+   private Tegel currentRoom, nextRoom; 
+   private boolean isGone = false;
    
-   private Tegel currentRoom, nextRoom;// room in now, next room 
-   private boolean isGone = false;// is the muur there
-   
-
-   // Two constructors will be created
-   // which will account for muurs with or 
-   // without neighbors
-
-   // with a neighbor 
-   public Muur(Tegel huidige, Tegel buur){
+   //Constructor tegel met buur
+   public Muur(Tegel huidige, Tegel buur)
+   {
       currentRoom = huidige;
       nextRoom = buur;
    }
 
-   // without a neighbor
-   public Muur(Tegel r){
+   //Constructor tegel zonder buur
+   public Muur(Tegel r)
+   {
       currentRoom = r;
       nextRoom = null;
    }
 
-
-    public Tegel getCurrentRoom() {
+    public Tegel getCurrentRoom()
+    {
         return currentRoom;
     }
 
-    public void setCurrentRoom(Tegel currentRoom) {
+    public void setCurrentRoom(Tegel currentRoom)
+    {
         this.currentRoom = currentRoom;
     }
 
-    public boolean getIsGone() {
+    public boolean getIsGone()
+    {
         return isGone;
     }
 
-    public void setIsGone(boolean isGone) {
+    public void setIsGone(boolean isGone)
+    {
         this.isGone = isGone;
     }
 
-    public Tegel getNextRoom() {
+    public Tegel getNextRoom()
+    {
         return nextRoom;
     }
 
-    public void setNextRoom(Tegel nextRoom) {
+    public void setNextRoom(Tegel nextRoom)
+    {
         this.nextRoom = nextRoom;
     }
 }
